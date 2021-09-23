01319781eb68463ebbbd75b4aeb65266b36f39b2
kiku99 <elsd0326@gmail.com>
2021-09-23 21:17:48 +0900

    Enemy.java 생성

---
+++ src/moon_lander/Enemy.java
@@ -0,0 +1,43 @@
+package moon_lander;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+
+public class Enemy {
+    //적의 x 좌표
+    public int x;
+    //적의 y 좌표
+    public int y;
+
+    public Enemy(int HP, int moveSpeed){
+        Initialize();
+        LoadContent();
+    }
+
+    private void Initialize(){
+
+    }
+
+    private void LoadContent(){
+
+    }
+
+    public void ResetPlayer(){
+
+    }
+
+    public void Move(){
+
+    }
+
+}
--- src/moon_lander/Game.java
+++ src/moon_lander/Game.java
@@ -10,6 +10,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
+
 import static moon_lander.Framework.*;
 import static moon_lander.Framework.*;
 

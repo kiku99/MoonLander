3ead45bfd9987fdb6d84479c42be92d350a0a807
kiku99 <elsd0326@gmail.com>
2021-09-23 22:05:44 +0900

    위에서 떨어지는 적 구현

--- src/moon_lander/Enemy.java
+++ src/moon_lander/Enemy.java
@@ -14,30 +14,64 @@ import javax.imageio.ImageIO;
 
 
 public class Enemy {
+
+    //적의  랜덤 x, y 좌표룰 생성하기 위한 변수
+    private Random random;
     //적의 x 좌표
     public int x;
     //적의 y 좌표
     public int y;
+    //적의 x 축 속도
+    private int speedX;
+    //적의 y 축 속도
+    private int speedY;
+    //적 이미지
+    private BufferedImage enemyImg;
+    //적 파괴시 이미지
+    private BufferedImage enemyCrashedImg;
+    //적이 파괴된 상태
+    public boolean crashed;
 
     public Enemy(int HP, int moveSpeed){
         Initialize();
         LoadContent();
+
+        x = random.nextInt(10);
     }
 
     private void Initialize(){
+        random = new Random();
 
+        ResetPlayer();
     }
 
     private void LoadContent(){
+        try {
+            URL enemyImgUrl = this.getClass().getResource("/resources/images/rocket.png");
+            enemyImg = ImageIO.read(enemyImgUrl);
 
+            URL enemyCrashedImgUrl = this.getClass().getResource("/resources/images/rocket.png");
+            enemyCrashedImg = ImageIO.read(enemyCrashedImgUrl);
+        } catch (IOException ex) {
+            Logger.getLogger(PlayerRocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void ResetPlayer(){
+        crashed = false;
 
+        x = random.nextInt(700);
+        y = -100;
+
+        speedY = 1;
     }
 
     public void Move(){
-
+        this.y += this.speedY;
+        //적이 화면을 넘어가면 위치 초기화
+        if(this.y > 500){
+            this.y = -100;
+        }
     }
 
 }

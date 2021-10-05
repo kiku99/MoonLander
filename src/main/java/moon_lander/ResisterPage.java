53dbce6da7aebdeb876af75882abcef669f64d4f
HanSH010101 <hsh8538@daum.net>
2021-10-05 17:24:23 +0900

    스테이지

--- src/main/java/moon_lander/ResisterPage.java
+++ src/main/java/moon_lander/ResisterPage.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import sun.security.mscapi.*;
 
 public class ResisterPage extends JFrame {
     private JTextField tResisterID;
